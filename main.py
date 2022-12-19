4d8179f5eb2174c76e246b86a17bca7230ebfc1c
NotFound404 <n0tx404@proton.me>
2022-12-18 21:50:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,29 +7,40 @@ bot_token = os.environ['TELEGRAM_BOT_TOKEN']
 
 bot = telebot.TeleBot(bot_token, parse_mode=None)
 
+def build_menu(buttons,n_cols,header_buttons=None,footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons)
+    if footer_buttons:
+        menu.append(footer_buttons)
+    return menu
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Send code <Python>")
 
+
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    # Usar un intérprete de Python aislado para ejecutar el código enviado por el usuario
-    with sandbox.Sandbox():
-        # Deshabilitar ciertas funciones peligrosas
-        sandbox.disable_import()
-        sandbox.disable_open()
-        sandbox.disable_eval()
+    try:
+        my_code = message.text
+        old_stdout = sys.stdout
+        redirected_output = sys.stdout = StringIO()
 
-        # Validar y sanear el código enviado por el usuario
-        my_code = sandbox.validate_and_clean(message.text)
-
-        # Ejecutar el código enviado por el usuario
+        bot.send_message(message.chat.id, parse_mode= 'Markdown')
         try:
-            output = os.system(my_code)
-            if output=='':
-                output='Empty result🤷🏻‍♀️🤷🏻‍♂️'
-            bot.send_message(message.chat.id, text=output)
+            exec(my_code)
         except Exception as e:
-            bot.send_message(message.chat.id, text='⚠️: '+str(e))
+            print('\n⚠️: '+str(e))
+        sys.stdout = old_stdout
+        output = redirected_output.getvalue()
+        if output=='': output='Empty result🤷🏻‍♀️🤷🏻‍♂️'
+        #print (output)
+        bot.send_message(message.chat.id,text=output)
+    except Exception as e:
+        bot.send_message(message.chat.id,text='⚠️: '+e)
+    finally:
+        bot.send_message(message.chat.id, parse_mode= 'Markdown')
+
 
 bot.infinity_polling()
